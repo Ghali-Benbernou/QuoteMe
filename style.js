880a81d2ab42b6f0b2dbb78b795b20879cc0a716
GHALI <ghali.benbernou@gisb.dz>
2021-01-07 14:47:35 +0100

    initial commit

---
+++ style.js
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+  justifyContent:"flex-end",
+    padding: 8,
+    height:50  },
+    
+  paragraph: {
+    margin: 0,
+    fontSize: 18,
+    fontWeight: 'normal',
+    fontStyle: 'italic',
+    textAlign: 'center',
+    color: 'white'
+  },
+  image:{
+    marginTop:40,
+    paddingTop:60,
+    width: 150,
+    height: 200,
+    alignSelf:"center",
+  },
+  text:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#111d5e'
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white'
+  },
+  
+});
