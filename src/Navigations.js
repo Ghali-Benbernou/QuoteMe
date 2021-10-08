65f272a9981a30c25ba15c8ede9c3c68be1f2517
benbernou ilias <iliasbenbernou741@gmail.com>
2021-10-08 17:32:18 +0200

    add costum drawel

--- src/Navigations.js
+++ src/Navigations.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {NavigationContainer} from '@react-navigation/native';
-import Home from './Screens/Home';
-import Categories from './Screens/Categories';
+import React from "react";
+import { View, Text } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./Screens/Home";
+import Categories from "./Screens/Categories";
+import Drawel from "./Screens/Drawel";
 
 const Drawer = createDrawerNavigator();
 
-export default function Navigations() {
+const Navigations = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      {/* <Drawer.Navigator
         initialRouteName="Home"
         drawerContentOptions={{
           style: {backgroundColor: '#111d5e'},
           activeTintColor: 'white',
           inactiveTintColor: 'white',
-        }}>
+        }}> */}
+      <Drawer.Navigator
+        drawerContent={() => <Drawel />}
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="About" component={Categories} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
-}
+};
+export default Navigations;
